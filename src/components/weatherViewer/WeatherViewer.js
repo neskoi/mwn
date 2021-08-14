519449f4464a5e659b648aff7624419ccc73b4e4
Koi Nes <jan.nes@fatec.sp.gov.br>
2021-08-14 16:25:09 -0300

    feat: o background muda conforme a previsao do clima

--- src/components/weatherViewer/WeatherViewer.js
+++ src/components/weatherViewer/WeatherViewer.js
@@ -7,7 +7,7 @@ import DateElement from './dateElement/DateElement';
 
 const WeatherViewer = ({data}) => (
     <div className="weather-viewer">
-        <div className={`weather-img ${data.weather?.main.toLowerCase()}`} title={`${data.weather?.main}`}/>
+        <div className={`weather-img ${data.weather?.main.toLowerCase()}`} title={`${data.weather?.description}`}/>
         <div className="temp"></div>
         <Temperatures data={data}/>
         <CityName className="city-name" cityName={data?.name}/>
--- src/page/Page.js
+++ src/page/Page.js
@@ -1,9 +1,13 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import './page.css'
 import Nav from '../components/nav/Nav';
-import WeatherViewer from '../components/weatherViewer/WeatherViewer'
+import WeatherViewer from '../components/weatherViewer/WeatherViewer';
+import bkgs from '../assets/bkgs.json';
+
 const Page = (props) => {
+    //const background = useRef("noimgbro");
     const [weatherInfo, setWeatherInfo] = useState({});
+    const [background, setBackground] = useState("/default.jfif");
 
     useEffect(() => {
         if("geolocation" in navigator){
@@ -11,7 +15,8 @@ const Page = (props) => {
                 const latitude = position.coords.latitude;
                 const longitude = position.coords.longitude;
                 const apiKey = process.env.REACT_APP_APIKEY;
-                const request = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${apiKey}`;
+                const language = navigator.language.slice(0,2);
+                const request = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${apiKey}&lang=${language}`;
                 fetch(request)
                     .then(response => response.json())
                     .then(json => {
@@ -23,13 +28,19 @@ const Page = (props) => {
         }
     }, []);
 
+    useEffect(() => {
+        const weather = weatherInfo.weather?.main.toLowerCase() || null;
+        if (weather === null) return;
+        const bkgsAmount = bkgs[weather].length - 1;
+        const choosedBkg = Math.round(Math.random() * bkgsAmount);
+        setBackground(bkgs[weather][choosedBkg]);
+    }, [weatherInfo]);
 
     return (
-        <>
+        <section className="page" style={{backgroundImage: `url(${background})`}}>
             <Nav/>
             <WeatherViewer data={weatherInfo}/>
-            <div></div>
-        </>
+        </section>
     )
 }
 
